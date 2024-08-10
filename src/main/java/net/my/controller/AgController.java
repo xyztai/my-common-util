72af765a6c04452c8739eded5d1a38ff54ceb195
xyztai <taixyz@163.com>
2024-08-10 08:29:06 +0800

    Update AgController.java

--- src/main/java/net/my/controller/AgController.java
+++ src/main/java/net/my/controller/AgController.java
@@ -164,30 +164,8 @@ public class AgController {
         if(CollectionUtils.isEmpty(opers)) {
             return RestGeneralResponse.of("无操作");
         }
-        StringBuilder stringBuilder = new StringBuilder();
-        Comparator<String> comp = (String::compareTo);
-        List<String> times = opers.stream().map(AgOper::getTime).distinct().sorted(comp.reversed()).collect(Collectors.toList());
-        Map<String, Map<String, List>> retMap = new LinkedHashMap<>();
-        for(String time : times) {
-            Map<String, List> timeMap = new LinkedHashMap<>();
-            List<String> buyList = opers.stream().filter(f -> time.equals(f.getTime()) && "buy".equals(f.getOperDir()))
-                    .map(m -> m.getName() + ": " + m.getBuyOper().replaceAll("buy", "买")).collect(Collectors.toList());
-            if(!CollectionUtils.isEmpty(buyList)) {
-                timeMap.put("买:", buyList);
-            }
 
-            List<String> sellList = opers.stream().filter(f -> time.equals(f.getTime()) && "sell".equals(f.getOperDir()))
-                    .map(m -> m.getName() + ": " + m.getSellOper().replaceAll("sell", "卖")).collect(Collectors.toList());
-            if(!CollectionUtils.isEmpty(sellList)) {
-                timeMap.put("卖:", sellList);
-            }
-
-            if(!CollectionUtils.isEmpty(timeMap)) {
-                retMap.put(time, timeMap);
-            }
-        }
-
-        return RestGeneralResponse.of(retMap);
+        return RestGeneralResponse.of(makeMap(opers));
     }
 
     @GetMapping("/oper/simple")
@@ -197,10 +175,21 @@ public class AgController {
         if(CollectionUtils.isEmpty(opers)) {
             return RestGeneralResponse.of("无操作");
         }
-        StringBuilder stringBuilder = new StringBuilder();
+
+        return RestGeneralResponse.of(makeMap(opers));
+    }
+
+    private Map<String, Map<String, List>> makeMap(List<AgOper> opers) {
+        Map<String, Map<String, List>> retMap = new LinkedHashMap<>();
+        if(CollectionUtils.isEmpty(opers)) {
+            return retMap;
+        }
+
         Comparator<String> comp = (String::compareTo);
         List<String> times = opers.stream().map(AgOper::getTime).distinct().sorted(comp.reversed()).collect(Collectors.toList());
-        Map<String, Map<String, List>> retMap = new LinkedHashMap<>();
+        if(!times.contains("9999-99-99")) {
+            retMap.put("预期操作", null);
+        }
         for(String time : times) {
             Map<String, List> timeMap = new LinkedHashMap<>();
             List<String> buyList = opers.stream().filter(f -> time.equals(f.getTime()) && "buy".equals(f.getOperDir()))
@@ -216,11 +205,14 @@ public class AgController {
             }
 
             if(!CollectionUtils.isEmpty(timeMap)) {
-                retMap.put(time, timeMap);
+                if("9999-99-99".equals(time)) {
+                    retMap.put("预期操作", timeMap);
+                } else {
+                    retMap.put(time, timeMap);
+                }
             }
         }
-
-        return RestGeneralResponse.of(retMap);
+        return retMap;
     }
 
 }
