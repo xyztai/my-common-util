0d3a245f8a8a7d636d5bff5b32637171c8ecdd1f
xyztai <taixyz@163.com>
2024-08-24 08:51:52 +0800

    modify

--- src/main/java/net/my/controller/AgController.java
+++ src/main/java/net/my/controller/AgController.java
@@ -165,7 +165,7 @@ public class AgController {
 
         List<String> retList = new ArrayList<>();
         if(!CollectionUtils.isEmpty(bos)) {
-            retList = bos.stream().map(m -> String.format("%s: expma5=%.0f, expma37=%.0f, sRatio=%.4f%s(para=%.3f, pre=%.4f), bRatio=%.4f%s(para=%.3f, pre=%.4f)"
+            retList = bos.stream().map(m -> String.format("%s: expma5=%.2f, expma37=%.2f, sRatio=%.4f%s(para=%.3f, pre=%.4f), bRatio=%.4f%s(para=%.3f, pre=%.4f)"
                     , m.getName(), m.getExpma5(), m.getExpma37()
                     , m.getSRatio(), m.getSRatio()>=1 && m.getSRatio() >= m.getSRatioPre() ? " ↑↑↑ " : ""
                     , m.getSRatioPara(), m.getSRatioPre()
