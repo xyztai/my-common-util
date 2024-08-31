9bc399d443d3ecd304879933f8249eca83c95132
xyztai <taixyz@163.com>
2024-08-31 08:36:37 +0800

    Update sqls.sql

--- docs/sqls.sql
+++ docs/sqls.sql
@@ -8,3 +8,65 @@ and t1.time = t2.time
 and t2.`time` = t3.`time`
 and t2.`type` = t3.`type`
 -- and t1.type = 'hs300'
+
+
+    insert into t_history_expect(time, type, close_price, b_action, s_action)
+select
+    t11.`time`
+     -- , case when t11.b_ratio_ans - 1 >= b_ratio_500 then 'buy' else 'sell' end as `oper_dir`
+     -- , case when t11.b_ratio_ans - 1 >= b_ratio_500 then t11.b_ratio_ans else t11.s_ratio_ans end ratio_c
+     , t13.type
+     -- , t13.name
+     -- , t11.b_ratio_ans, t12.b_ratio_500
+     , t14.close_price
+     , case when t11.b_ratio_ans - 1 >= b_ratio_5000 then 5000
+            when t11.b_ratio_ans - 1 >= b_ratio_3000 then 3000
+            when t11.b_ratio_ans - 1 >= b_ratio_2000 then 2000
+            when t11.b_ratio_ans - 1 >= b_ratio_1000 then 1000
+            when t11.b_ratio_ans - 1 >= b_ratio_500 then  500
+            else null
+    end as `b_action`
+     -- , abs(t11.s_ratio_ans - 1), s_ratio_1_in_4_start
+     , case when t11.s_ratio_ans - 1 >= s_ratio_1_in_1 then -1
+            when t11.s_ratio_ans - 1 >= s_ratio_1_in_2 then -0.75
+            when t11.s_ratio_ans - 1 >= s_ratio_1_in_3 then -0.5
+            when t11.s_ratio_ans - 1 >= s_ratio_1_in_4 then -0.25
+            else null
+    end as `s_action`
+from
+    (
+        select
+            t1.`time`
+             , t1.`type`
+             , round(t1.`expma_5`/t1.`expma_37`, 5) s_ratio_ans
+             , round(t1.`expma_37`/t1.`expma_5`, 5) b_ratio_ans
+        from t_data_calc t1
+    ) t11,
+    (
+        select
+            t2.type
+             , t2.time
+             , 0.7*(t2.b_ratio-1) b_ratio_500
+             , 0.9*(t2.b_ratio-1) b_ratio_1000
+             , (t2.b_ratio-1) b_ratio_2000
+             , 1.1*(t2.b_ratio-1) b_ratio_3000
+             , 1.3*(t2.b_ratio-1) b_ratio_5000
+             , -0.01 s_ratio_1_in_4
+             , 0.3*(t2.s_ratio-1) s_ratio_1_in_3
+             , 0.7*(t2.s_ratio-1) s_ratio_1_in_2
+             , 1*(t2.s_ratio-1) s_ratio_1_in_1
+        from t_para_daily t2
+    ) t12,
+    (
+        select *
+        from t_type t3
+    ) t13,
+    t_raw_close_price t14
+where t11.type = t12.type
+  and t11.time = t12.time
+  and t11.type = t13.type
+  and t11.time = t14.time
+  and t11.type = t14.type
+  and ((t11.b_ratio_ans - 1 >= b_ratio_500) or (t11.s_ratio_ans - 1 >= s_ratio_1_in_4))
+order by 1 desc, 2, 3 desc
+
