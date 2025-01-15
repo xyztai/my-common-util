1eb297c6173eda7e60447083f419075e78f70713
xyztai <taixyz@163.com>
2025-01-15 12:37:15 +0800

    Update MyCaffeineCache.java

--- src/main/java/net/my/cache/MyCaffeineCache.java
+++ src/main/java/net/my/cache/MyCaffeineCache.java
@@ -14,7 +14,7 @@ public class MyCaffeineCache extends AbstractCaffeineCache {
     @Override
     LoadingCache createLoadingCache() {
         loadingCache = Caffeine.newBuilder()
-                .expireAfterWrite(30L, TimeUnit.MINUTES) // 最后一次写入后，经过固定时间过期
+                .expireAfterWrite(2L, TimeUnit.MINUTES) // 最后一次写入后，经过固定时间过期
                 .initialCapacity(10) // 初始的缓存空间大小
                 .maximumSize(100) // 缓存的最大条数
                 .recordStats() // 开发统计功能
