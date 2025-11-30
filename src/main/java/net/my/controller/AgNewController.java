ed3e1351c551425e59ee53b6f743b29fd337a375
xyztai <taixyz@163.com>
2025-11-30 08:41:01 +0800

    Update AgNewController.java

--- src/main/java/net/my/controller/AgNewController.java
+++ src/main/java/net/my/controller/AgNewController.java
@@ -120,11 +120,13 @@ public class AgNewController {
 
     @Data
     public class QqNode{
+        private String date;
         private Double open;
         private Double last;
         private Double high;
         private Double low;
         private Double volume;
         private Double amount;
+        private Double exchangeRaw;
     }
 }
