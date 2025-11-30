c922c7026d77b8a69335ecb112a829a2ab584b6d
xyztai <taixyz@163.com>
2025-11-30 09:41:20 +0800

    修改

--- src/main/java/net/my/mapper/DataCalcMapper.java
+++ src/main/java/net/my/mapper/DataCalcMapper.java
@@ -64,5 +64,5 @@ public interface DataCalcMapper {
     void updateFactor(@Param("buyFactor") String buyFactor, @Param("sellFactor") String sellFactor);
 
     int saveQqNode(QqNode qqNode);
-    int saveQqNodes(@Param("qqNodes") List<QqNode> qqNodes);
+    int saveQqNodes(List<QqNode> qqNodes);
 }
