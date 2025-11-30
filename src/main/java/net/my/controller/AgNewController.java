c38d6f4782297b06386d4947b57b9b385eabd478
xyztai <taixyz@163.com>
2025-11-30 13:40:28 +0800

    Update AgNewController.java

--- src/main/java/net/my/controller/AgNewController.java
+++ src/main/java/net/my/controller/AgNewController.java
@@ -168,7 +168,9 @@ public class AgNewController {
         BigDecimal bigDecimalLastValue = BigDecimal.valueOf(lastValue);
         BigDecimal bigDecimalCp = BigDecimal.valueOf(cp);
         return bigDecimalLastValue.add(
-                bigDecimalCp.subtract(bigDecimalLastValue).multiply(new BigDecimal("2.0").divide(new BigDecimal(step + 1))))
+                bigDecimalCp.subtract(bigDecimalLastValue)
+                        .multiply(new BigDecimal("2.0"))
+                        .divide(new BigDecimal(step + 1)))
                 .doubleValue();
     }
 
