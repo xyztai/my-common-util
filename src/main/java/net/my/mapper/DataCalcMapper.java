ec1a02cd6d473a6293cd9a0479e05aea811b4786
xyztai <taixyz@163.com>
2025-12-04 04:11:59 +0800

    Update DataCalcMapper.java

--- src/main/java/net/my/mapper/DataCalcMapper.java
+++ src/main/java/net/my/mapper/DataCalcMapper.java
@@ -67,5 +67,5 @@ public interface DataCalcMapper {
     int saveQqNodes(List<QqNode> qqNodes);
     QqNode getMaxQqNode(@Param("stockCode") String stockCode);
     List<SpecialCarePoJo> specialCare(@Param("time") String time);
-    List<String> getLatestDates(@Param("time") Integer days);
+    List<String> getLatestDates(@Param("days") Integer days);
 }
