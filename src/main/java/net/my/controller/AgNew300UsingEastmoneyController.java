b60308f727c9d1155210b14290f2ad8398802e03
xyztai <taixyz@163.com>
2025-12-23 17:48:25 +0800

    Update AgNew300UsingEastmoneyController.java

--- src/main/java/net/my/controller/AgNew300UsingEastmoneyController.java
+++ src/main/java/net/my/controller/AgNew300UsingEastmoneyController.java
@@ -196,6 +196,8 @@ public class AgNew300UsingEastmoneyController {
         // 更新expma字段
         log.info("阶段2-99999数据-开始更新expma字段");
         updateExpma();
+        log.info("删除 delEastMoneyBuy99999");
+        dataCalcMapper.delEastMoneyBuy99999();
         // 更新buy表
         List<String> needCalcBuys = dataCalcMapper.getNeedCalcDates("2025-06-01", "3");
         if(!CollectionUtils.isEmpty(needCalcBuys)) {
