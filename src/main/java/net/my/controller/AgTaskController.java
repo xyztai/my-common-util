9c348e1883f7512901b90ac6557c2288c90b2b1d
xyztai <taixyz@163.com>
2026-01-03 09:56:50 +0800

    Create AgTaskController.java

---
+++ src/main/java/net/my/controller/AgTaskController.java
@@ -0,0 +1,30 @@
+package net.my.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import net.my.config.ScheduledTasks;
+import net.my.pojo.BaseResponse;
+import net.my.pojo.RestGeneralResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/ag-task")
+@Slf4j
+public class AgTaskController {
+
+    @Autowired
+    private ScheduledTasks scheduledTasks;
+
+    @GetMapping("/trigger")
+    public BaseResponse trigger() {
+        log.info("trigger start");
+        scheduledTasks.execGetHistoryDataNew();
+        log.info("trigger end");
+        return RestGeneralResponse.OK;
+    }
+
+}
+
