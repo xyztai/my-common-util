7d6bda40d74cba5dcf50f01fcb327f725e8bae84
xyztai <taixyz@163.com>
2026-01-08 07:03:57 +0800

    quer9Zhuan

--- src/main/java/net/my/mapper/AgEastmoneyStockMapper.java
+++ src/main/java/net/my/mapper/AgEastmoneyStockMapper.java
@@ -25,5 +25,6 @@ public interface AgEastmoneyStockMapper {
     List<SpecialCarePoJo2> queryEastmoneyLast30();
     List<SpecialCarePoJo2> queryEastmoneyVolSuddenlyRised();
     List<SpecialCarePoJo2> queryEastmoneyVolSuddenlyRisedToday();
+    List<SpecialCarePoJo2> quer9Zhuan();
     List<SpecialCarePoJo2> queryEastmoneyLatestInfo();
 }
