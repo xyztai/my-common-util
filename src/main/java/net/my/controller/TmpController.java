31c56c6aaf6c1b7d77dd3a0b69f5de454f78da41
xyztai <taixyz@163.com>
2026-02-08 04:09:42 +0800

    add tmp

---
+++ src/main/java/net/my/controller/TmpController.java
@@ -0,0 +1,37 @@
+package net.my.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import net.my.mapper.TmpMapper;
+import net.my.pojo.BaseResponse;
+import net.my.pojo.RestGeneralResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/avg")
+@Slf4j
+public class TmpController {
+
+    @Autowired
+    private TmpMapper tmpMapper;
+
+    @GetMapping("/calc/calcDate")
+    public BaseResponse calc(@PathVariable("calcDate") String calcDate) {
+        log.info("calc start");
+        List<String> dates = tmpMapper.getDates(calcDate);
+        if(!CollectionUtils.isEmpty(dates)) {
+            for(String d : dates) {
+                log.info("calc calcD={}", calcDate);
+                tmpMapper.calcAvg(d);
+            }
+        }
+        log.info("calc end");
+        return RestGeneralResponse.OK;
+    }
+}
---
+++ src/main/java/net/my/mapper/TmpMapper.java
@@ -0,0 +1,12 @@
+package net.my.mapper;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TmpMapper {
+    List<String> getDates(@Param("calcDate") String calcDate);
+    void calcAvg(@Param("calcDate") String calcDate);
+}
